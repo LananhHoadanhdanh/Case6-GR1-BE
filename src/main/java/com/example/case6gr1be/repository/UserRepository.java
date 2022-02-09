0b8e6731f153a8324676387d387e05b24c1f6d6e
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2022-02-09 08:22:18 +0700

    user repo

---
+++ src/main/java/com/example/case6gr1be/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.case6gr1be.repository;
+
+import com.example.case6gr1be.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
