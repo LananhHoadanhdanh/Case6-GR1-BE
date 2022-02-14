275519d88106cb95e36be9bab1277b138e486a92
tekdoken <tekdokenzx@gmail.com>
2022-02-14 13:54:02 +0700

    mode order

---
+++ src/main/java/com/example/case6gr1be/model/Order.java
@@ -0,0 +1,92 @@
+package com.example.case6gr1be.model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Timestamp bookingTime;
+    private Timestamp startTime;
+    private Timestamp endTime;
+    @ManyToOne
+    private OrderStatus status;
+    @ManyToOne
+    @JoinColumn(name = "provider_id")
+    private User provider;
+    @ManyToOne
+    @JoinColumn(name = "renter_id")
+    private User renter;
+
+    public Order() {
+    }
+
+    public Order(Timestamp bookingTime, Timestamp startTime, Timestamp endTime, OrderStatus status, User provider, User renter) {
+        this.bookingTime = bookingTime;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
+        this.provider = provider;
+        this.renter = renter;
+    }
+
+    public Timestamp getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(Timestamp bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+    public Timestamp getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getProvider() {
+        return provider;
+    }
+
+    public void setProvider(User provider) {
+        this.provider = provider;
+    }
+
+    public User getRenter() {
+        return renter;
+    }
+
+    public void setRenter(User renter) {
+        this.renter = renter;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
