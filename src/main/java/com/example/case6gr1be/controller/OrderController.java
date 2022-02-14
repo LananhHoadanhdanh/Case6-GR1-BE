a8813ab4654251808bc009f1b309cf6c242c6028
tekdoken <tekdokenzx@gmail.com>
2022-02-14 15:19:43 +0700

    controller Order saveOrder

--- src/main/java/com/example/case6gr1be/controller/OrderController.java
+++ src/main/java/com/example/case6gr1be/controller/OrderController.java
@@ -1,12 +1,31 @@
 package com.example.case6gr1be.controller;
 
+import com.example.case6gr1be.model.Order;
+import com.example.case6gr1be.service.OrderService;
+import com.example.case6gr1be.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
 @PropertySource("classpath:application.properties")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/orders")
+    public ResponseEntity<Iterable<Order>> findAll() {
+        return new ResponseEntity<>(orderService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/orders")
+    public void save(@RequestBody Order order) {
+        orderService.save(order);
+    }
 
 }
