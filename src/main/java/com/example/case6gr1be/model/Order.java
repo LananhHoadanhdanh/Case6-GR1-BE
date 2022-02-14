472cc97870f606ff3d5fff5b9325008f5550b2a5
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2022-02-14 14:17:30 +0700

    tạo bảng order

--- src/main/java/com/example/case6gr1be/model/Order.java
+++ src/main/java/com/example/case6gr1be/model/Order.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
+@Table(name = "order_table")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
