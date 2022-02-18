1b5da0b30c6c0c099adc5d3f8046acb3f1b1dcf8
ak88vp <tiendung2020japan@gmail.com>
2022-02-18 10:12:42 +0700

    a

--- src/main/java/com/example/case6gr1be/service/impl/RepostStatusServiceImpl.java
+++ src/main/java/com/example/case6gr1be/service/impl/RepostStatusServiceImpl.java
@@ -3,12 +3,14 @@ package com.example.case6gr1be.service.impl;
 import com.example.case6gr1be.model.ReportStatus;
 import com.example.case6gr1be.repository.ReportStatusRepository;
 import com.example.case6gr1be.service.ReportStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class RepostStatusServiceImpl implements ReportStatusService {
+    @Autowired
     private ReportStatusRepository reportStatusRepository;
 
     @Override
